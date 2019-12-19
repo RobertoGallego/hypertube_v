eccaf40c1a6b9a8fad81a2955582dcc8c19ca7ca
RobertoGallego <groberto@student.42.fr>
2019-12-19 10:43:12 +0100

    ok

--- ui/src/components/SignUp/header.js
+++ ui/src/components/SignUp/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logoTop from '../../img/logo-top.png';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Img = styled.img`
     width: 15%;
@@ -10,7 +11,7 @@ const Img = styled.img`
 export default function Header() {
     return (
         <div>
-            <Img src={logoTop} alt='Netflix' />
+            <Link to="/"><Img src={logoTop} alt='Netflix' /></Link>
         </div>
     );
 }
